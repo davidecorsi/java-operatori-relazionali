4b6d9f5a0aaa2230de2df3fa713c4d4835a891d8
Davide Corsi <davide.corsi@par-tec.it>
2021-09-30 15:58:26 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+	public static void main(String[] args) {
+		// operatori relazionali
+		
+		5 == 5 true
+		5 == 3 false
+		System.out.println(5 == 5);
+		
+		int eta = 22;
+		boolean maggiorenne = eta >= 18;
+		System.out.println(maggiorenne);
+	}
+}
